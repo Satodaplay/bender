4610b29ae573e0a692c3af3636aa4996775ec6fa
Satodaplay <danielo4canal@gmail.com>
2025-03-05 22:34:45 +0100

    teleport arreglao

--- src/Bender.java
+++ src/Bender.java
@@ -6,7 +6,6 @@ class Bender {
     private char[][] mapaVisual;
     private RobotBender robot;
     private boolean inversorActivado = false;
-
     private char[] direcciones = {'S', 'E', 'N', 'W'};
     private int[][] movimiento = {
             {1, 0},  // S
@@ -44,7 +43,6 @@ class Bender {
                 mapaArray[i][j] = ' ';
             }
         }
-
         return mapaArray;
     }
 
@@ -68,48 +66,39 @@ class Bender {
         Set<String> visitado = new HashSet<>();
         while (true) {
             boolean moved = false;
-            String estadoActual = robot.getX() + "," + robot.getY() + "," + String.valueOf(inversorActivado) + direcciones[0];
+            // Include the complete 'direcciones' array in the state
+            String estadoActual = robot.getX() + "," + robot.getY() + "," + inversorActivado + "," + Arrays.toString(direcciones);
             if (visitado.contains(estadoActual)) {
-                return null;  // Bucle detectado
+                return null;  // Loop detected
             }
             visitado.add(estadoActual);
-
             for (int i = 0; i < 4; i++) {
                 int nuevoX = robot.getX() + movimiento[i][0];
                 int nuevoY = robot.getY() + movimiento[i][1];
-
                 if (esPosicionValida(nuevoX, nuevoY)) {
                     robot.mover(nuevoX, nuevoY);
                     camino.append(direcciones[i]);
-
                     if (mapaVisual[nuevoX][nuevoY] == '$') {
                         return camino.toString();
                     }
-
                     if (mapaVisual[nuevoX][nuevoY] == 'T') {
                         teleport();
                     }
-
                     if (mapaVisual[nuevoX][nuevoY] == 'I') {
                         inversorActivado = !inversorActivado;
                         invertirDirecciones();
                     }
-
                     moverDireccionAlFrente(i);
                     moved = true;
                     break;
                 }
             }
-
             if (!moved) {
                 break;
             }
         }
         return camino.toString();
     }
-
-// Es necesario añadir String.valueOf(inversorActivado) para incluir el estado del inversor en la cadena de visitado,
-// lo que evita bucles no detectados.
 
     private void invertirDirecciones() {
         if (inversorActivado) {
@@ -124,12 +113,10 @@ class Bender {
     private void moverDireccionAlFrente(int index) {
         char selectedDir = direcciones[index];
         int[] selectedMove = movimiento[index];
-
         for (int i = index; i > 0; i--) {
             direcciones[i] = direcciones[i - 1];
             movimiento[i] = movimiento[i - 1];
         }
-
         direcciones[0] = selectedDir;
         movimiento[0] = selectedMove;
     }
@@ -140,39 +127,47 @@ class Bender {
             System.out.println("No hay puntos de teletransporte disponibles.");
             return;
         }
-
-        int[] tpMasCercano = null;
-        double distanciaMinima = Double.MAX_VALUE;
         int currentX = robot.getX();
         int currentY = robot.getY();
-
-        // Direcciones en "orden de reloj" cuando las distancias son iguales: NE, SE, SW, NW
-        int[][] relojPrioridad = {{-1, 1}, {1, 1}, {1, -1}, {-1, -1}};
-
+        double distanciaMinima = Double.MAX_VALUE;
+        int[] tpMasCercano = null;
+        int minPriority = Integer.MAX_VALUE;
         for (int[] tp : teleports) {
+            if (tp[0] == currentX && tp[1] == currentY) {
+                continue; // Omitir la posición actual
+            }
             double distancia = calcularDistancia(currentX, currentY, tp[0], tp[1]);
-            if (!Arrays.equals(tp, new int[]{currentX, currentY})) {
-                if (distancia < distanciaMinima) {
+            if (distancia < distanciaMinima) {
+                distanciaMinima = distancia;
+                tpMasCercano = tp;
+                minPriority = getPriority(currentX, currentY, tp[0], tp[1]);
+            } else if (distancia == distanciaMinima) {
+                int priority = getPriority(currentX, currentY, tp[0], tp[1]);
+                if (priority < minPriority) {
                     tpMasCercano = tp;
-                    distanciaMinima = distancia;
-                } else if (distancia == distanciaMinima) {
-                    for (int[] prioridad : relojPrioridad) {
-                        int prioridadX = currentX + prioridad[0];
-                        int prioridadY = currentY + prioridad[1];
-                        if (tp[0] == prioridadX && tp[1] == prioridadY) {
-                            tpMasCercano = tp;
-                            break;
-                        }
-                    }
+                    minPriority = priority;
                 }
             }
         }
-
         if (tpMasCercano != null) {
             robot.mover(tpMasCercano[0], tpMasCercano[1]);
             System.out.println("Teletransportado a: (" + tpMasCercano[0] + ", " + tpMasCercano[1] + ")");
         } else {
             System.out.println("No se encontró un teletransporte diferente disponible para moverse.");
+        }
+    }
+
+    private int getPriority(int currentX, int currentY, int tpX, int tpY) {
+        if (tpX < currentX && tpY > currentY) {
+            return 1; // NE
+        } else if (tpX > currentX && tpY > currentY) {
+            return 2; // SE
+        } else if (tpX > currentX && tpY < currentY) {
+            return 3; // SW
+        } else if (tpX < currentX && tpY < currentY) {
+            return 4; // NW
+        } else {
+            return 5; // Otros casos
         }
     }
 
